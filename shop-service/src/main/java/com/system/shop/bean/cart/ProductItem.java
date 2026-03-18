6bc1e609bd8f82e30bed2b7e175be69f3362fa1a
zhanghaijun <604836658@qq.com>
2026-03-18 17:10:37 +0800

    1

---
+++ shop-service/src/main/java/com/system/shop/bean/cart/ProductItem.java
@@ -0,0 +1,19 @@
+package com.system.shop.bean.cart;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+public class ProductItem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String productCode;
+    private String skuCode;
+    private String productName;
+    private BigDecimal price;
+    private Integer quantity;
+    private boolean selected = false;  // 默认未选中
+}
